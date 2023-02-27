08226f694009c5a118649d80e51afaa011530cdc
Jason Martinez <jasonmar08@gmail.com>
2023-02-27 15:49:50 -0500

    update config file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import { Provider  } from 'mobx-react'
-import { observer,  } from 'mobx-react'
+import { Provider } from 'mobx-react'
+import { observer } from 'mobx-react'
 
 import AppStore from './store'
-import colors from 'tailwindcss/colors' 
+import colors from 'tailwindcss/colors'
 import {
   BrowserRouter as Router,
   Switch,
   Redirect,
-  Route,
-} from "react-router-dom";
+  Route
+} from 'react-router-dom'
 
 import Header from './Header'
 import Search from './Search'
@@ -26,60 +26,69 @@ import Login from './Login/Login'
 import Profile from './Profile/'
 import LoginSuccess from './Login/Success'
 
-
 import './App.scss'
 
-if(!window.store){
-  window.store = new AppStore();
+if (!window.store) {
+  window.store = new AppStore()
 }
 
-
+console.log('ENV:', process.env.NODE_ENV)
+console.log('CONFIG:', config.baseURL)
 @observer
 class App extends Component {
   render() {
     return (
-    <ThemeProvider theme={colors}>
+      <ThemeProvider theme={colors}>
         <Provider store={window.store}>
           <Router>
-           {window.store.redirect ? <Redirect to={window.store.redirect} /> : null }
-            {window.store.isLoggedIn ? <>
-            {window.store.profile.status ? <>  {/*  Logged in with plan */}
-                <Switch>
-                    <Route path="/writing/document"><div/></Route>
-                    <Route component={Header} />
-                </Switch>
-               
-                <Switch>
-                      
+            {window.store.redirect ? (
+              <Redirect to={window.store.redirect} />
+            ) : null}
+            {window.store.isLoggedIn ? (
+              <>
+                {window.store.profile.status ? (
+                  <>
+                    {' '}
+                    {/*  Logged in with plan */}
+                    <Switch>
+                      <Route path="/writing/document">
+                        <div />
+                      </Route>
+                      <Route component={Header} />
+                    </Switch>
+                    <Switch>
                       <Route path="/" exact component={Dashboard} />
                       <Route path="/search" exact component={Search} />
 
-
-                      <Route path="/ai/" >
+                      <Route path="/ai/">
                         <Switch>
-
                           <Route path="/ai/code/debugging" component={Chat} />
                           <Route component={Tool} />
-
                         </Switch>
                       </Route>
                       <Route path="/my-profile" component={Profile} />
                       <Route path="/signup/failed" component={Profile} />
                       <Route path="/signup/success" component={LoginSuccess} />
                       <Route path="/signup/success" component={LoginSuccess} />
-                    
-                  </Switch>
-                </> : <> {/* Logged in but no plan */}
-               
-                </>} </> : <> {/*  Not Logged In */}
+                    </Switch>
+                  </>
+                ) : (
+                  <> {/* Logged in but no plan */}</>
+                )}{' '}
+              </>
+            ) : (
+              <>
+                {' '}
+                {/*  Not Logged In */}
                 <Switch>
                   <Route path="/" exact>
                     <Redirect to="/login" />
                   </Route>
                   <Route path="/" component={Login} />
                 </Switch>
-            </>}
-           </Router>
+              </>
+            )}
+          </Router>
         </Provider>
       </ThemeProvider>
     )
--- src/config.js
+++ src/config.js
@@ -1,25 +1,23 @@
 const dev = {
-	baseURL: "http://localhost:3080/api/",
-	landingPageUrl: "http://localhost:3080",
-	stripe: {
-		free: "price_1JcQD6HIZYvvnKladKayEvOZ",
-		entry: "price_1JL68HHIZYvvnKlaBJWS5uDe",
-		pro: "price_1JLQhlHIZYvvnKlakrqF8khB"
-	},
-};
-  
+  baseURL: 'https://nutty-woolens-lamb.cyclic.app/api/',
+  landingPageUrl: 'https://nutty-woolens-lamb.cyclic.app',
+  stripe: {
+    free: 'price_1JcQD6HIZYvvnKladKayEvOZ',
+    entry: 'price_1JL68HHIZYvvnKlaBJWS5uDe',
+    pro: 'price_1JLQhlHIZYvvnKlakrqF8khB'
+  }
+}
+
 const prod = {
-	baseURL: '/api/',
-	landingPageUrl: "https://app.openaitemplate.com",
-	stripe: {
-		free: "price_1JcQsUHIZYvvnKlaEPy958NF",
-		entry: "price_1JLpWpHIZYvvnKlaHxiCr7Js",
-		pro: "price_1JLpWxHIZYvvnKlamATZ7Awm"
-	},
-};
-  
-const config = process.env.NODE_ENV === 'development'
-	? dev
-	: prod;
-  
-export default config;
+  baseURL: '/api/',
+  landingPageUrl: 'https://app.openaitemplate.com',
+  stripe: {
+    free: 'price_1JcQsUHIZYvvnKlaEPy958NF',
+    entry: 'price_1JLpWpHIZYvvnKlaHxiCr7Js',
+    pro: 'price_1JLpWxHIZYvvnKlamATZ7Awm'
+  }
+}
+
+const config = process.env.NODE_ENV === 'development' ? dev : prod
+
+export default config
